05cb35b1a9081b05ffeabaaceb33147ccd268038
Muhamad Ndaru <muhamad.ndaru@gmail.com>
2023-06-26 00:19:18 +0700

    fix: unique index email in user's entity

--- src/domains/users/entities/user.go
+++ src/domains/users/entities/user.go
@@ -12,7 +12,7 @@ type User struct {
 	ID   string `json:"id" gorm:"primaryKey;type:varchar(255)"`
 	Name string `json:"name" gorm:"type:varchar(255)"`
 	//Email    string  `json:"email" gorm:"unique,type:varchar(255)" validate:"unique_email=id"`
-	Email    string  `json:"email" gorm:"unique,type:varchar(255)"`
+	Email    string  `json:"email" gorm:"index:email,unique,type:varchar(255)"`
 	Password string  `json:"password" gorm:"type:varchar(255)"`
 	GoogleID *string `json:"google_id" gorm:"unique,type:varchar(255)"`
 	entities.Timestamp
--- src/main.go
+++ src/main.go
@@ -11,5 +11,8 @@ func main() {
 	app := gin.Default()
 	RegisterMiddlewares(app)
 	RegisterRoutes(app)
-	app.Run(fmt.Sprintf(":%d", appConfig.GetConfig().App.Port))
+	err := app.Run(fmt.Sprintf(":%d", appConfig.GetConfig().App.Port))
+	if err != nil {
+		return
+	}
 }
